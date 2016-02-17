cbaf51cebf9dde48846e5e17da5a5990e96a791e
wolferen <veliar.master@gmail.com>
2016-02-17 18:40:08 +0200

    validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,10 @@
 class Task < ActiveRecord::Base
 belongs_to :user
 enum priority: [:high, :medium, :low]
-	
-	validates :title, 
-          	:presence => {notice: "Title can't be blank." },
-          	:uniqueness => {notice: "Title already exists."},
-          	:length => {:minimum => 4, :maximum => 60, notice: "Must be more that 4 and less than 60 characters"}
+
+validates :title, 
+           :presence => {message: "Task can't be blank." },
+           :length => {:minimum => 4, :maximum => 60, message: "Task must be more than 4 and less than 60 characters."}
 
 	def priority= value
 	  if value.kind_of?(String) and value.to_i.to_s == value
